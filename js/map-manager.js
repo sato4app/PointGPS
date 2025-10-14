e62865b32e195b7d7f319a31e2f625b9be39b9eb
sato4app <sato.apps@quad.razor.jp>
2025-10-14 21:56:21 +0900

    add top-left zoom-button: bug fix

--- js/map-manager.js
+++ js/map-manager.js
@@ -26,10 +26,15 @@ export class MapManager {
             metric: true,
             imperial: false
         }).addTo(this.map);
-        
+
         // ズームコントロールを左上に配置
         L.control.zoom({
             position: 'topleft'
+        }).addTo(this.map);
+
+        // ズームコントロールを右下にも配置
+        L.control.zoom({
+            position: 'bottomright'
         }).addTo(this.map);
 
         console.log('地図を初期化しました');
