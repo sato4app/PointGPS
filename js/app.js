03207c8614a07d9bc5485cb6b44391b2f1365af6
sato4app <sato.apps@quad.razor.jp>
2025-11-01 13:16:16 +0900

    add help button: new window size=1200x800

--- js/app.js
+++ js/app.js
@@ -123,7 +123,7 @@ class PointGPSApp {
         const helpBtn = document.getElementById('helpBtn');
 
         helpBtn.addEventListener('click', () => {
-            window.open('PointGPS-help.html', '_blank', 'width=800,height=600,scrollbars=yes,resizable=yes');
+            window.open('PointGPS-help.html', '_blank', 'width=1200,height=800,scrollbars=yes,resizable=yes');
         });
 
         // ポイント情報フィールドの変更イベント
